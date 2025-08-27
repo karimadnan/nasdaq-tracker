25e6c98f243fc148095eb81b6eec8b6b949d76aa
karimadnan <karimmadnan@gmail.com>
2025-08-28 00:28:23 +0300

    feat: cards grid component for web

---
+++ modules/ui/src/lib/components/cards-grid/cards-grid.tsx
@@ -0,0 +1,20 @@
+import { Fragment, ReactNode } from 'react';
+import { XStack } from 'tamagui';
+
+type Props<T> = {
+  keyExtractor: (record: T) => React.Key;
+  renderItem: (record: T, index: number) => ReactNode;
+  data: T[];
+};
+
+export function CardsGrid<T>({ data, keyExtractor, renderItem }: Props<T>) {
+  return (
+    <XStack flexWrap="wrap" gap="$4" justify="center">
+      {data.map((record, index) => (
+        <Fragment key={keyExtractor(record)}>
+          {renderItem(record, index)}
+        </Fragment>
+      ))}
+    </XStack>
+  );
+}
