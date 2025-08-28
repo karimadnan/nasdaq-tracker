4cc3896de4e2332880c0cd9d79fd09a1cc25d5cc
karimadnan <karimmadnan@gmail.com>
2025-08-28 23:33:13 +0300

    feat: add react router and outlet logic

--- apps/nasdaq-tracker-web/src/app/app.tsx
+++ apps/nasdaq-tracker-web/src/app/app.tsx
@@ -6,13 +6,21 @@ import { StocksPage } from './components/stocks-page/stocks-page';
 import { SearchInput } from './components/top-bar/top-bar-search-input';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './query-client';
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router/dom';
+
+const router = createBrowserRouter([
+  {
+    element: <TopBar leading={<TopBarLogo />} content={<SearchInput />} />,
+    children: [{ path: '/', element: <StocksPage /> }],
+  },
+]);
 
 export function App() {
   return (
     <TamaguiProvider config={tamagUiConfig} defaultTheme="light">
       <QueryClientProvider client={queryClient}>
-        <TopBar leading={<TopBarLogo />} content={<SearchInput />} />
-        <StocksPage />
+        <RouterProvider router={router} />,
       </QueryClientProvider>
     </TamaguiProvider>
   );
--- modules/ui/src/lib/components/top-bar/top-bar.tsx
+++ modules/ui/src/lib/components/top-bar/top-bar.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import { XStack, View } from 'tamagui';
+import { Outlet } from 'react-router';
 
 type Props = {
   leading: ReactNode;
@@ -9,27 +10,30 @@ type Props = {
 
 export function TopBar({ leading, content, trailing }: Props) {
   return (
-    <XStack
-      justify="space-between"
-      background="grey"
-      marginBlockEnd="$5"
-      paddingBlock="$5"
-      flexWrap="wrap"
-      borderBottomRightRadius={30}
-      borderBottomLeftRadius={30}
-      gap={15}
-    >
-      <View>{leading}</View>
-      <View
-        $lg={{ width: 700 }}
-        width="100%"
-        maxW={'100%'}
-        justify="center"
-        paddingHorizontal={15}
+    <>
+      <XStack
+        justify="space-between"
+        background="grey"
+        marginBlockEnd="$5"
+        paddingBlock="$5"
+        flexWrap="wrap"
+        borderBottomRightRadius={30}
+        borderBottomLeftRadius={30}
+        gap={15}
       >
-        {content}
-      </View>
-      <View>{trailing}</View>
-    </XStack>
+        <View>{leading}</View>
+        <View
+          $lg={{ width: 700 }}
+          width="100%"
+          maxW={'100%'}
+          justify="center"
+          paddingHorizontal={15}
+        >
+          {content}
+        </View>
+        <View>{trailing}</View>
+      </XStack>
+      <Outlet />
+    </>
   );
 }
