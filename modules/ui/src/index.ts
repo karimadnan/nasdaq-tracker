6dd311451073d07f08d579dbfdf033eeac3e81ca
karimadnan <karimmadnan@gmail.com>
2025-08-28 23:07:18 +0300

    chore: export skeleton components

--- modules/ui/src/index.ts
+++ modules/ui/src/index.ts
@@ -1,4 +1,6 @@
 export { StocksCard } from './lib/components/stocks-card/stocks-card';
 export { CardsGrid } from './lib/components/cards-grid/cards-grid';
 export { TopBar } from './lib/components/top-bar/top-bar';
+export { Skeleton, SkeletonCard } from './lib/components/skeleton/skeleton';
+
 export { tamagUiConfig } from './tamagui.config';
