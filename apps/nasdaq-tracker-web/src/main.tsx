d44b62a1116672589bdfddcec476948b5e2c40d1
karimadnan <karimmadnan@gmail.com>
2025-08-28 23:09:07 +0300

    chore: create api client for web

--- apps/nasdaq-tracker-web/src/main.tsx
+++ apps/nasdaq-tracker-web/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './app/app';
+import { createApi } from '@org/api';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+createApi(import.meta.env.VITE_API_URL, import.meta.env.VITE_API_TOKEN);
 
 root.render(
   <StrictMode>
